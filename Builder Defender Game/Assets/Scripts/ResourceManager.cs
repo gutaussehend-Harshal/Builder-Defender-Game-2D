5b92f28d7de584f6db735df9dba85e4234131882
Harshal Patil <harshal.amrut@gmail.com>
2021-08-01 18:05:49 +0530

    Added Resource Generator

--- Builder Defender Game/Assets/Scripts/ResourceManager.cs
+++ Builder Defender Game/Assets/Scripts/ResourceManager.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
+    public static ResourceManager Instance { get; private set; }
+
+    // private static ResourceManager instance;
+    // public static ResourceManager GetInstance()
+    // {
+    //     return instance;
+    // }
+    // private static void SetInstance(ResourceManager set)
+    // {
+    //     instance = set;
+    // }
     private Dictionary<ResourceTypeSO, int> resourcesAmountDictionary;
 
-    private void Awake() 
+    private void Awake()
     {
-        resourcesAmountDictionary  = new Dictionary<ResourceTypeSO, int>();
+        Instance = this;
+
+        resourcesAmountDictionary = new Dictionary<ResourceTypeSO, int>();
         ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
 
-        foreach (ResourceTypeSO resourceType in resourceTypeList.list) 
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
         {
             resourcesAmountDictionary[resourceType] = 0;
         }
@@ -19,9 +32,9 @@ public class ResourceManager : MonoBehaviour
         TestLogResourceAmountDictionary();
     }
 
-    private void Update() 
+    private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.T)) 
+        if (Input.GetKeyDown(KeyCode.T))
         {
             ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
             AddResource(resourceTypeList.list[0], 2);
@@ -29,16 +42,17 @@ public class ResourceManager : MonoBehaviour
         }
     }
 
-    private void TestLogResourceAmountDictionary() 
+    private void TestLogResourceAmountDictionary()
     {
-        foreach (ResourceTypeSO resourceType in resourcesAmountDictionary.Keys) 
+        foreach (ResourceTypeSO resourceType in resourcesAmountDictionary.Keys)
         {
             Debug.Log(resourceType.nameString + ": " + resourcesAmountDictionary[resourceType]);
         }
     }
 
-    public void AddResource(ResourceTypeSO resourceType, int amount) 
+    public void AddResource(ResourceTypeSO resourceType, int amount)
     {
         resourcesAmountDictionary[resourceType] += amount;
+        TestLogResourceAmountDictionary();
     }
 }
